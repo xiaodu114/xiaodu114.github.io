462d340d13895f2bbdb35ca849e4f411c62123a0
杜东志 <dudongzhi@live.com>
2020-04-13 23:01:52 +0800

    修改 查询语句

--- DB/Mongodb/sample1/query.js
+++ DB/Mongodb/sample1/query.js
@@ -1,78 +1,67 @@
 //https://xbuba.com/questions/36824601
-db.getCollection('IRM_ModelConfig').aggregate([{
-        $match: {
-            $or: [{
-                    ModelId: '334256284381220864',
-                    AppCode: null,
-                    BusinessModuleId: null,
-                    OrganizationId: null,
-                    SceneCode: null
-                },
-                {
-                    ModelId: '334256284381220864',
-                    AppCode: "OperationControlCooperation",
-                    BusinessModuleId: "0e69aae9-8587-11e8-8d4d-00155d0a1925",
-                    OrganizationId: null,
-                    SceneCode: null
-                },
-                {
-                    ModelId: '334256284381220864',
-                    AppCode: "OperationControlCooperation",
-                    BusinessModuleId: "0e69aae9-8587-11e8-8d4d-00155d0a1925",
-                    OrganizationId: "53305584107157536",
-                    SceneCode: null
-                },
-                {
-                    ModelId: '334256284381220864',
-                    AppCode: "OperationControlCooperation",
-                    BusinessModuleId: "0e69aae9-8587-11e8-8d4d-00155d0a1925",
-                    OrganizationId: "53305584107157536",
-                    SceneCode: "1"
-                }
-            ]
+db.getCollection('ConfigData').aggregate([{
+    $match: {
+        $or: [{
+            FormId: 'CarInfo',
+            Level1: null,
+            Level2: null,
+            Level3: null,
+        }, {
+            FormId: 'CarInfo',
+            Level1: "pineapple",
+            Level2: null,
+            Level3: null,
+        }, {
+            FormId: 'CarInfo',
+            Level1: "pineapple",
+            Level2: "koala",
+            Level3: null,
+        }, {
+            FormId: 'CarInfo',
+            Level1: "pineapple",
+            Level2: "koala",
+            Level3: "cucumber",
+        }]
+    },
+}, {
+    $sort: {
+        CreateDate: -1
+    }
+}, {
+    $group: {
+        _id: {
+            FormId: "$FormId",
+            Level1: "$Level1",
+            Level2: "$Level2",
+            Level3: "$Level3",
         },
-    },
-    {
-        $group: {
-            _id: {
-                ModelId: "$ModelId",
-                AppCode: "$AppCode",
-                BusinessModuleId: "$BusinessModuleId",
-                OrganizationId: "$OrganizationId",
-                SceneCode: "$SceneCode"
-            },
-            latestRecord: {
-                $first: "$$ROOT"
-            }
-        }
-    },
-    {
-        $sort: {
-            _id: -1
-        }
-    },
-    {
-        $replaceRoot: {
-            newRoot: "$latestRecord"
-        }
-    },
-    {
-        $group: {
-            _id: {
-                ModelId: "$ModelId"
-            },
-            items: {
-                $addToSet: "$$ROOT"
-            }
-        }
-    },
-    {
-        $project: {
-            _id: 0,
-            ModelId: '$_id.ModelId',
-            latestConfig: {
-                $arrayElemAt: ["$items", 0]
-            }
+        latestRecord: {
+            $first: "$$ROOT"
         }
     }
-])
+}, {
+    $sort: {
+        _id: -1
+    }
+}, {
+    $replaceRoot: {
+        newRoot: "$latestRecord"
+    }
+}, {
+    $group: {
+        _id: {
+            FormId: "$FormId"
+        },
+        items: {
+            $push: "$$ROOT"
+        }
+    }
+}, {
+    $project: {
+        _id: 0,
+        FormId: '$_id.FormId',
+        latestConfig: {
+            $arrayElemAt: ["$items", 0]
+        }
+    }
+}]);
