4e9350c92d61edbff15dee43b6820be97ddfa7b3
杜东志 <dudongzhi@live.com>
2021-02-06 19:11:18 +0800

    添加 回到顶部 组件

--- WebComponents/typesetting/js/typesetting.js
+++ WebComponents/typesetting/js/typesetting.js
@@ -153,3 +153,123 @@ customElements.define('inline-code',
         }
     }
 );
+
+customElements.define('back-to-top',
+    class BackToTop extends HTMLElement {
+
+        constructor() {
+            super();
+            const shadowRoot = this.attachShadow({
+                mode: 'open'
+            });
+
+            //#region 组件模板
+            shadowRoot.innerHTML = `
+            <style>
+                .ddz-typesetting-backtotop-container{
+                    display: none;
+                    position: fixed;
+                    bottom: 15px;
+                    right: 15px;
+                    background-color: #3cb878;
+                    color: #fff;
+                    cursor: pointer;
+                    font-size: 18px;
+                    padding: 5px 10px;
+                }
+
+                .ddz-typesetting-backtotop-container>div{
+                    display: inline-block;
+                }
+            </style>
+            <div class="ddz-typesetting-backtotop-container" title="\u8fd4\u56de\u9876\u90e8">
+                <div class="ddz-typesetting-backtotop-icon">&uarr;</div>
+                <div class="ddz-typesetting-backtotop-text"></div>
+            </div>
+            `;
+            //#endregion
+
+            //  这里获取不到传入的属性值
+            //  可以获取dom结构
+            this.containerDom = this.shadowRoot.querySelector(".ddz-typesetting-backtotop-container");
+            this.backtotopTextDom = this.shadowRoot.querySelector(".ddz-typesetting-backtotop-container>.ddz-typesetting-backtotop-text");
+            this.monitorEle = null;
+            this.scrollEle = null;
+            this.handleMonitorEleScroll = this.handleMonitorEleScroll.bind(this);
+            this.goBackToTop = this.goBackToTop.bind(this);
+        }
+
+        //#region  生命周期回调
+
+        /**
+         *  当自定义元素第一次被连接到文档DOM时被调用（没有参数）
+         */
+        connectedCallback() {
+            let scrollSelectorValue = this.getAttribute("scroll-selector");
+            if (scrollSelectorValue == null) {
+                this.scrollEle = document.documentElement;
+                if (document.documentElement && typeof document.documentElement.scrollTop === "number") {
+                    this.scrollEle = document.documentElement;
+                } else if (document.body.parentNode && typeof document.body.parentNode.scrollTop === "number") {
+                    this.scrollEle = document.body.parentNode;
+                }
+                this.monitorEle = window;
+            } else {
+                try {
+                    this.monitorEle = document.querySelector(scrollSelectorValue);
+                } catch (error) {
+                    throw new Error(JSON.stringify(error));
+                }
+                if (!this.monitorEle) throw new Error(`根据${scrollSelectorValue}未能找到元素！`);
+                // this.monitorEle.style.position = "relative";
+                this.containerDom.style.position = "sticky";
+                this.containerDom.style.left = "100%";
+                this.containerDom.style.marginRight = "15px";
+                this.scrollEle = this.monitorEle;
+            };
+            this.monitorEle.addEventListener("scroll", this.handleMonitorEleScroll);
+            this.containerDom.addEventListener("click", this.goBackToTop);
+        }
+
+        /**
+         *  当自定义元素与文档DOM断开连接时被调用
+         */
+        disconnectedCallback() {
+            this.monitorEle.removeEventListener('scroll', this.handleMonitorEleScroll);
+            this.containerDom.removeEventListener('click', this.goBackToTop);
+            this.monitorEle = null;
+            this.scrollEle = null;
+        }
+
+        /**
+         *  当自定义元素被移动到新文档时被调用
+         */
+        adoptedCallback() {
+            console.log("adoptedCallback");
+        }
+
+        /**
+         * 当自定义元素的一个属性被增加、移除或更改时被调用
+         */
+        attributeChangedCallback(attrName, oldValue, newValue) {}
+
+        //#endregion
+
+        //#region   私有方法（想弄成私有，但是不知道怎么弄,你当做不能访问就行了）
+        goBackToTop() {
+            if (!this.scrollEle) return;
+            this.scrollEle.scrollTop = 0;
+        }
+
+        handleMonitorEleScroll(evt) {
+            if (!(typeof this.scrollEle.scrollTop === "number" && typeof this.scrollEle.scrollHeight === "number" && typeof this.scrollEle.clientHeight === "number")) return;
+            if (this.scrollEle.scrollTop === 0) {
+                this.containerDom.style.display = "none";
+            } else {
+                this.containerDom.style.display = "inline-block";
+                this.backtotopTextDom.innerHTML = ((this.scrollEle.scrollTop / (this.scrollEle.scrollHeight - this.scrollEle.clientHeight)) * 100).toFixed(0) + "%";
+            }
+        }
+        //#endregion
+    }
+);
