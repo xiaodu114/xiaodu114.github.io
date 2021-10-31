5dfc5e15c261c1189c365a25d3f010c5a80742a5
杜东志 <dudongzhi@live.com>
2021-10-31 20:45:32 +0800

    vue3 入门更新添加 单文件组件支持

--- p/vue3/0introduction/code/module1/js/index.js
+++
@@ -1,8 +0,0 @@
-import * as Vue from "vue";
-Vue.createApp({
-    data() {
-        return {
-            msg: "Hi，这里是 Vue3 + webpack 多入口 示例项目 模块1 入口页面"
-        }
-    }
-}).mount('#vue3Module1Entry');
--- p/vue3/0introduction/code/module2/js/index.js
+++
@@ -1,8 +0,0 @@
-import * as Vue from "vue";
-Vue.createApp({
-    data() {
-        return {
-            msg: "Hi，这里是 Vue3 + webpack 多入口 示例项目 模块2 入口页面"
-        }
-    }
-}).mount('#vue3Module2Entry');
