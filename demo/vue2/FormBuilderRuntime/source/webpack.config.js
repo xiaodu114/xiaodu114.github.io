1ad66ac5a07309588fddfb2f1bb1349dfbdbb5bb
xiaodu <ddz_job@outlook.com>
2022-06-12 10:18:06 +0800

    vue2.x 示例项目再次迁移

---
+++ demo/vue2/FormBuilderRuntime/source/webpack.config.js
@@ -0,0 +1,69 @@
+//  依赖
+//  npm i deep-extend vue@2
+//  开发依赖 
+//  npm i buffer copy-webpack-plugin css-loader html-webpack-plugin less less-loader style-loader vue-loader@15 vue-template-compiler webpack webpack-cli -D
+
+const path = require('path');
+const webpack = require("webpack");
+//  现在换成 webpack5 之后，编译之后报错了："ReferenceError: Buffer is not defined"
+//      依赖 deep-extend 中有一个这样的方法：function isSpecificValue(val) { return (val instanceof Buffer || val instanceof Date || val instanceof RegExp) ? true : false; }
+//      之前使用 webpack4 的时候，同样使用 deep-extend 没有报错
+//      解决办法：https://viglucci.io/how-to-polyfill-buffer-with-webpack-5
+//const Buffer = require('buffer/').Buffer;
+const htmlWebPack = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+    //  开发模式下使用
+    //mode: 'development',    // production | development  
+    //devtool: "source-map",
+    //  产品模式下使用
+    //      说明：按理说产品模式下应该启用压缩，但是在产品模式下用"copy-webpack-plugin"拷贝的文件也会被压缩（如果被压缩会有一些问题，例如：demo/vue2/DashboardByGridStack/source/src/copy/htmlSnippets/amd3/module1.js   Uncaught (in promise) TypeError: define is not a function）
+    mode: 'production',    // production | development  
+    optimization: {
+        minimize: false
+    },
+    entry: {
+        "app": './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        //  path 和 publicPath 这两个根据实际情况自己调整一下
+        path: path.resolve(__dirname, '../www'),
+        publicPath: '/demo/vue2/FormBuilderRuntime/www/'
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: ['vue-loader']
+        }, {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'less-loader']
+        },]
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        },
+        fallback: {
+            buffer: require.resolve('buffer/'),
+        },
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new VueLoaderPlugin(),
+        //  因为要上传到Github中，copy文件夹（弄了一个压缩包保留）已删除，具体的请到输出目录"www"中查看
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: 'src/copy',
+                to: ''
+            },]
+        }),
+        new htmlWebPack({
+            inject: "body",
+            template: './src/index.html'
+        })
+    ]
+}
---
+++ demo/vue2/MainProject/source/webpack.config.js
@@ -0,0 +1,220 @@
+//  依赖 
+//  npm i @babel/runtime @babel/runtime-corejs3 element-ui lodash vue@2
+//  开发依赖 
+//  npm i @babel/core @babel/plugin-transform-runtime @babel/preset-env babel-loader clean-webpack-plugin copy-webpack-plugin css-loader file-loader html-loader html-webpack-plugin less less-loader mini-css-extract-plugin style-loader vue-loader@15 vue-template-compiler webpack webpack-cli -D
+
+const path = require('path');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPack = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+let multiPageConfig = [{
+    entry: {
+        // key中以'/'分割，最后一项是文件的名称，前面的都是目录
+        targetPath: "demo/component/float-menu/single-stage/js/index",
+        sourcePath: "./src/demo/component/float-menu/single-stage/js/index.js"
+    },
+    htmlWebPack: {
+        sourcePath: './src/demo/component/float-menu/single-stage/index.html',
+        targetPath: `demo/component/float-menu/single-stage/index.html`
+    }
+},
+{
+    entry: {
+        targetPath: "demo/component/layout/wordtable/js/index",
+        sourcePath: "./src/demo/component/layout/wordtable/js/index.js"
+    },
+    htmlWebPack: {
+        sourcePath: './src/demo/component/layout/wordtable/index.html',
+        targetPath: `demo/component/layout/wordtable/index.html`
+    }
+}
+];
+
+let tempModuleExportsObj = {
+    mode: 'production', // production | development
+    //devtool: "source-map",
+    entry: {},
+    output: {
+        filename: '[name].js',
+        //  path 和 publicPath 这两个根据实际情况自己调整一下
+        path: path.resolve(__dirname, '../www'),
+        publicPath: '/demo/vue2/MainProject/www/'
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: ['vue-loader']
+        }, {
+            test: /\.html$/,
+            use: ['html-loader']
+        }, {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }, {
+            test: /\.less$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    //  第二种方式
+                    //      dependencies: @babel/runtime @babel/runtime-corejs3
+                    //      devDependencies: @babel/plugin-transform-runtime  @babel/preset-env
+                    presets: [
+                        ["@babel/preset-env"]
+                    ],
+                    plugins: [
+                        ['@babel/plugin-transform-runtime', {
+                            corejs: {
+                                version: 3,
+                                proposals: true
+                            },
+                            useESModules: true
+                        }]
+                    ]
+                }
+            }
+        },
+        //  记得以前是这样实现的
+        // {
+        //     test: /\.(jpg|jpeg|png|gif|svg|eot|svg|ttf|woff|woff2)$/,
+        //     loader: 'file-loader',
+        //     options: {
+        //         // 如果不设置 vue文件中引入的图片会出现这样的错误<img src="[object Module]">
+        //         esModule: false,
+        //         name(file) {
+        //             //  项目中引用了element-ui库，该类库中有包含字体文件
+        //             //  这里使用 path.relative(path.join(__dirname, "src"), file).split(path.sep).join("/") 得到的路径为绝对路径(盘符的根目录开始，例如：F:\)
+        //             //  你的项目目录 + \node_modules\element-ui\lib\theme-chalk\fonts\element-icons.ttf
+        //             //  所以这里加上如下判断，当然如果你的好多node_modules包中都包含字体文件或者图片等，可能就需要换种写法了
+        //             if (/[\\/]node_modules[\\/](element-ui)[\\/]/gi.test(file)) {
+        //                 return path.join("lib/element-ui", path.basename(file)).split(path.sep).join("/");
+        //             } else {
+        //                 return path.relative(path.join(__dirname, "src"), file).split(path.sep).join("/");
+        //             }
+        //         }
+        //     }
+        // },
+        {
+            test: /\.(jpg|jpeg|png|gif|svg|eot|svg|ttf|woff|woff2)$/,
+            type: 'asset/resource',
+            generator: {
+                filename(a, b) {
+                    // a.filename 的值，例如： 'node_modules/element-ui/lib/theme-chalk/fonts/element-icons.woff'
+                    if (/node_modules[\\/](element-ui)[\\/]/gi.test(a.filename)) {
+                        return path.join("lib/element-ui", path.basename(a.filename)).split(path.sep).join("/");
+                    }
+                    else {
+                        return path.relative(path.join(__dirname, "src"), path.resolve(a.filename)).split(path.sep).join("/");
+                    }
+                }
+            }
+        }]
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    },
+    optimization: { // 优化项
+        splitChunks: { //分割代码块
+            cacheGroups: { // 缓存
+                // commons: { // 公共的代码
+                //     name: "common", // 抽离出来的模块名
+                //     chunks: 'initial', // 初始化，从入口文件开始抽离
+                //     minSize: 0, // 如果这个代码大于0字节
+                //     minChunks: 2, // 这个代码引用多少次才需要抽离
+                //     filename: 'common/js/[name].js'
+                // },
+                'self-common': { // 自己整理的通用方法
+                    name: "common/js/common", // 以'/'分割，最后一项是文件的名称，前面的都是目录
+                    chunks: 'initial',
+                    minSize: 0,
+                    minChunks: 2,
+                    filename: '[name].js'
+                },
+                lodash: { // 抽取第三方模块：lodash
+                    priority: 99,
+                    name: 'lodash',
+                    test: /[\\/]node_modules[\\/](lodash)[\\/]/,
+                    chunks: 'initial',
+                    minSize: 0,
+                    minChunks: 1,
+                    filename: 'lib/[name]/[name].js'
+                },
+                vue: { // 抽取第三方模块：vue
+                    priority: 98,
+                    name: 'vue',
+                    test: /[\\/]node_modules[\\/](vue)[\\/]/,
+                    chunks: 'initial',
+                    minSize: 0,
+                    minChunks: 1,
+                    filename: 'lib/[name]/[name].js'
+                },
+                elementui: { // 抽取第三方模块：element-ui
+                    priority: 88,
+                    name: 'lib/element-ui/element-ui',
+                    test: /[\\/]node_modules[\\/](element-ui)[\\/]/,
+                    chunks: 'initial',
+                    minSize: 0,
+                    minChunks: 1,
+                    filename: '[name].js'
+                }
+            }
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: 'src/**/*.json',
+                //to: 'mock'
+                to({
+                    context,
+                    absoluteFilename
+                }) {
+                    return Promise.resolve(path.relative(path.join(context, "src"), absoluteFilename).split(path.sep).join("/"));
+                },
+            },]
+        }),
+        new MiniCssExtractPlugin({
+            filename: ({
+                chunk
+            }) => `${chunk.name.replace('/js/', '/css/')}.css`,
+        }),
+        new VueLoaderPlugin()
+    ]
+};
+
+multiPageConfig.forEach((singlePage) => {
+    if (singlePage.entry && singlePage.entry.sourcePath) {
+        let tempEntryObj = {};
+        tempEntryObj[singlePage.entry.targetPath] = singlePage.entry.sourcePath;
+        Object.assign(tempModuleExportsObj.entry, tempEntryObj);
+    }
+    if (singlePage.htmlWebPack && singlePage.htmlWebPack.sourcePath) {
+        let tempHtmlWebPackOption = {
+            inject: true, // true | false | 'body'
+            hash: true, //开启hash  ?[hash]
+            chunks: [], //页面要引入的包
+        };
+        if (singlePage.entry && singlePage.entry.sourcePath) {
+            tempHtmlWebPackOption.chunks.push(singlePage.entry.targetPath);
+        }
+        Object.assign(tempHtmlWebPackOption, {
+            template: singlePage.htmlWebPack.sourcePath,
+            filename: singlePage.htmlWebPack.targetPath,
+        });
+        tempModuleExportsObj.plugins.push(
+            new HtmlWebPack(tempHtmlWebPackOption)
+        );
+    }
+});
+module.exports = tempModuleExportsObj;
