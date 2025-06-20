0ad954cc7291824f70f5ef1815b4d4caa73ee735
xiaodu <ddz_job@outlook.com>
2025-06-20 14:34:20 +0800

    vue 3 lowcode 支持二级菜单之路由逻辑

--- demo/vue3/lowcode/router/index.js
+++ demo/vue3/lowcode/router/index.js
@@ -7,18 +7,51 @@ const routerOptions = {
     history: createMemoryHistory(),
     routes: []
 };
+
 if (Array.isArray(menuItems) && menuItems.length) {
     menuItems.forEach((item) => {
-        routerOptions.routes.push({
-            name: item.code,
-            path: "/" + item.code,
-            component: () => import(item.componentPath)
-        });
+        if (!item.code) return;
 
-        items.push({
-            text: item.text,
-            routerName: item.code
-        });
+        if (item.isDirectory) {
+            const subItems = [];
+            if (Array.isArray(item.children) && item.children.length) {
+                item.children.forEach((subItem) => {
+                    if (!(subItem.code && subItem.componentPath)) return;
+                    routerOptions.routes.push({
+                        name: item.code + "." + subItem.code,
+                        path: "/" + item.code + "/" + subItem.code,
+                        component: () => import(subItem.componentPath)
+                    });
+                    subItems.push({
+                        id: subItem.id,
+                        text: subItem.text,
+                        routerName: item.code + "." + subItem.code
+                    });
+                });
+            }
+            if (subItems.length) {
+                items.push({
+                    id: item.id,
+                    text: item.text,
+                    isDirectory: true,
+                    children: subItems
+                });
+            }
+        } else {
+            if (item.componentPath) {
+                routerOptions.routes.push({
+                    name: item.code,
+                    path: "/" + item.code,
+                    component: () => import(item.componentPath)
+                });
+
+                items.push({
+                    id: item.id,
+                    text: item.text,
+                    routerName: item.code
+                });
+            }
+        }
     });
 }
 
