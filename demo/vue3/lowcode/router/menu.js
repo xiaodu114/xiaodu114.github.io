6734d0ddd7220d5337e5ab7a1b670ed76e95422c
xiaodu <ddz_job@outlook.com>
2025-06-20 14:35:02 +0800

    vue 3 lowcode 支持二级菜单之菜单数据

--- demo/vue3/lowcode/router/menu.js
+++ demo/vue3/lowcode/router/menu.js
@@ -1,12 +1,32 @@
 export default [
     {
-        id: 1,
+        id: "1",
+        text: "表单",
+        code: "form",
+        isDirectory: true,
+        children: [
+            {
+                id: "1-1",
+                text: "示例一",
+                code: "demo1",
+                componentPath: "../page/form/demo1.js"
+            },
+            {
+                id: "1-2",
+                text: "示例二",
+                code: "demo2",
+                componentPath: "../page/form/demo2.js"
+            }
+        ]
+    },
+    {
+        id: "2",
         text: "页面一",
         code: "page1",
         componentPath: "../page/page1.js"
     },
     {
-        id: 2,
+        id: "3",
         text: "页面二",
         code: "page2",
         componentPath: "../page/page2.js"
