e27a5133b59f55036049a83c7bd6893ee5094e05
xiaodu <ddz_job@outlook.com>
2025-06-25 22:33:52 +0800

    lowcode uiLib 更新

--- demo/vue3/lowcode/store/uiLib.js
+++ demo/vue3/lowcode/store/uiLib.js
@@ -1,9 +1,9 @@
 import { defineStore } from "pinia";
 
 export const useUILibStore = defineStore("uiLib", {
-  state: () => {
-    return {
-      lib: "element-plus",
-    };
-  },
+    state: () => {
+        return {
+            lib: "element-plus"
+        };
+    }
 });
