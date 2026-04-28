0315e3ca809b71dde120bf55e3fca28c35710834
xiaodu <ddz_job@outlook.com>
2026-04-28 18:18:20 +0800

    nav 导航支持移动端

---
+++ nav/components/svgIcon/arrowDown.js
@@ -0,0 +1,8 @@
+export default {
+    name: "svgIconArrowDown",
+    template: `<svg height="1rem"width="1rem" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <path d="M957.463608 326.608639 541.393447 753.98224l0 0-29.720905 30.527271-29.718859-30.527271 0 0L65.883523 326.608639l29.718859-30.525224 416.07016 427.370531 416.07016-427.370531L957.463608 326.608639z" />
+        </svg>`,
+    props: {},
+    setup() {}
+};
---
+++ nav/js/index.js
@@ -0,0 +1,61 @@
+const { ref, onMounted } = Vue;
+const { useRouter, useRoute } = VueRouter;
+export default {
+    name: "App",
+    template: "#appTemplate",
+    props: {
+        head: {
+            type: Object,
+            default: {
+                title: "大家好，我是 xiaodu114",
+                links: []
+            }
+        },
+        navItems: {
+            type: Array,
+            default: []
+        }
+    },
+    setup(props) {
+        // 生命周期钩子
+        onMounted(() => {
+            console.log("生命周期钩子：onMounted");
+        });
+
+        //  无需暴露的数据
+        const router = useRouter();
+        const route = useRoute();
+        let activeNavItem = null;
+
+        //  需要暴露的数据
+        let selectedKeys = ref([]);
+        let mobileMenuOpen = ref(false);
+
+        //  不需要暴露的方法
+        const init = () => {
+            const firstLeafItem = props.navItems[0];
+            selectedKeys.value = [firstLeafItem.key];
+            selectHandler({item:firstLeafItem, key: firstLeafItem.key, selectedKeys: [firstLeafItem.key]});
+        };
+
+        //  需要暴露的方法
+        function selectHandler({ item, key, selectedKeys }) {
+            if (activeNavItem === item) return;
+            activeNavItem = item;
+            router.push({ name: item.extend.routeName, params: { src: item.src } });
+            mobileMenuOpen.value = false;
+        }
+
+        function toggleMobileMenu() {
+            mobileMenuOpen.value = !mobileMenuOpen.value;
+        }
+
+        function closeMobileMenu() {
+            mobileMenuOpen.value = false;
+        }
+
+        init();
+
+        return { selectHandler, selectedKeys, mobileMenuOpen, toggleMobileMenu, closeMobileMenu };
+    }
+};
