5b33ce4984ee457351c51ca56d06d9bd5374da51
xiaodu <ddz_job@outlook.com>
2026-04-28 18:17:24 +0800

    先删除 nav,一会儿在上传一个支持移动端的

--- nav/components/index.js
+++
@@ -1,9 +0,0 @@
-import duMenu from "./menu.js";
-import iframeWrapper from "./iframeWrapper.js";
-import arrowDown from "./svgIcon/arrowDown.js";
-
-export function registerComponent(app) {
-    app.component(iframeWrapper.name, iframeWrapper);
-    app.component(duMenu.name, duMenu);
-    app.component(arrowDown.name, arrowDown);
-}
